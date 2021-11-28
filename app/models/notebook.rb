9c6fd106ff685af7cd4766492ce8cbfbf802c478
William Winzeler <williamwinzeler@macbook-pro-3.home>
2021-11-28 01:00:43 -0500

    added associations

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -2,5 +2,8 @@ class Notebook < ApplicationRecord
   validates :author_id, :title, presence: true
   validates :title, uniqueness: {scope: :author_id, message: "An Author must have unique notebooks"}
   
-  belongs_to :user
+  belongs_to :user, 
+    foreign_key: :author_id
+
+  has_many :notes 
 end
