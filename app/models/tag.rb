592a347235cace15cc768e396c596e1988badc1a
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2022-01-27 10:52:02 -0800

    cleanup

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,11 +7,4 @@ class Tag < ApplicationRecord
   validates :title, uniqueness: {scope: :user_id, 
     message: "user must have unique tag titles."}
 
-  validates :note_ids, length: { minimum: 1}, allow_nil: true 
-
-  def create_note_tags
-    next_id = Tag.connection.select_value("Select nextval('tags_id_seq')")
-    NotesTag.create!(tag_id: next_id, note_id: self.note_id)
-    self.note_id = null; 
-  end
 end
