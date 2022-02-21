bcc63bc14c3816b12c40ca5d5da24baf9b1c027e
Owen Winzeler <littlehouse2104@gmail.com>
2022-02-21 11:33:32 -0800

    prep for db migration

--- db/migrate/20220110192505_drop_note_tags.rb
+++ db/migrate/20220110192505_drop_note_tags.rb
@@ -1,7 +1,7 @@
-# class DropNoteTags < ActiveRecord::Migration[5.2]
-#   def change
-#     begin 
-#       drop_table :note_tags do 
-#     end
-#   end
-# end
+class DropNoteTags < ActiveRecord::Migration[5.2]
+  # def change
+  #   begin 
+  #     drop_table :note_tags do 
+  #   end
+  # end
+end
